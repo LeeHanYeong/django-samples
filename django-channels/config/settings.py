b6b67d141ba8c7ab24bad79e0f8bbfb5fac15e49
lhy <dev@lhy.kr>
2020-03-25 20:19:28 +0900

    Installation and default settings

--- django-channels/config/settings.py
+++ django-channels/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+ASGI_APPLICATION = 'config.routing.application'
 
 
 # Database
