62fab3e717dc60c6bb00e2fb98183c73a898e5f7
lhy <dev@lhy.kr>
2020-03-31 15:12:29 +0900

    rename drf-base64

--- drf-base64/drf_base64/fields.py
+++ drf-base64/drf_base64/fields.py
@@ -1,12 +1,12 @@
 from django.forms import ImageField
 from drf_extra_fields.fields import Base64FieldMixin, Base64ImageField, Base64FileField
 from rest_framework.exceptions import ValidationError
-from rest_framework.fields import FileField
+from rest_framework.fields import FileField, SkipField
 
 
 class Base64WithFilenameFieldMixin(Base64FieldMixin):
     INVALID_OBJECT = 'Must be an object containing keys "file_name" and "base64_data"'
-    INVALID_DATA = ''
+    INVALID_DATA = '"URL starting with http" or "Object with file_name and base64_data must be passed"'
     INVALID_FILE_NAME = 'The file name is incorrect. It should have the form "<name>.<extension>"'
     ALLOW_ALL_EXTENSIONS = False
 
@@ -16,30 +16,26 @@ class Base64WithFilenameFieldMixin(Base64FieldMixin):
             self.help_text = 'An object containing keys `file_name` and `base64_data`'
 
     def to_internal_value(self, obj):
-        if obj in (None, ''):
-            return obj
+        if obj in self.EMPTY_VALUES:
+            return None
+        elif isinstance(obj, str) and obj.startswith('http'):
+            raise SkipField()
+        elif isinstance(obj, dict):
+            if not all(key in obj for key in ('file_name', 'base64_data')):
+                raise ValidationError(self.INVALID_OBJECT)
 
-        if not all(key in obj for key in ('file_name', 'base64_data')):
-            if self.parent.instance:
-                # If an empty value or null is not passed, returns the existing value
-                if obj:
-                    origin_name = getattr(self.parent.instance, self.source).name
-                    return origin_name
-                else:
-                    return None
-            else:
-                return obj
+            try:
+                self.name, self.ext = obj['file_name'].rsplit('.', 1)
+            except ValueError:
+                raise ValidationError(self.INVALID_FILE_NAME)
 
-        try:
-            self.name, self.ext = obj['file_name'].rsplit('.', 1)
-        except ValueError:
-            raise ValidationError(self.INVALID_FILE_NAME)
+            if self.ext not in self.ALLOWED_TYPES and self.ALLOW_ALL_EXTENSIONS:
+                self.ALLOWED_TYPES.append(self.ext)
 
-        if self.ext not in self.ALLOWED_TYPES and self.ALLOW_ALL_EXTENSIONS:
-            self.ALLOWED_TYPES.append(self.ext)
-
-        base64_data = obj['base64_data']
-        return super().to_internal_value(base64_data)
+            base64_data = obj['base64_data']
+            return super().to_internal_value(base64_data)
+        else:
+            raise ValidationError(self.INVALID_DATA)
 
     def get_file_name(self, decoded_file):
         return self.name
